90d8746e566d15be7d2a25bb95d3df21a421b230
asarkar5 <ashoke.k.sarkar@gmail.com>
2019-03-25 19:53:21 -0400

    Updated

--- src/main/java/com/springboot_302aks/spboot302aks/HomeController.java
+++ src/main/java/com/springboot_302aks/spboot302aks/HomeController.java
@@ -3,8 +3,12 @@ package com.springboot_302aks.spboot302aks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -20,7 +24,13 @@ public class HomeController {
         model.addAttribute("job", new Job());
         return "jobform";
     }
-    
-
+    @PostMapping("/process")
+    public String processForm(@Valid Job job, BindingResult result){
+        if (result.hasErrors()){
+           return "jobform";
+        }
+        jobRepository.save(job);
+        return "redirect:/";
+    }
 
 }
